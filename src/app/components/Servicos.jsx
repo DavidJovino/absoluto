dd3546d649a4793cafc22e7d434620ada2db9bc3
David Jovino <davidjovino@live.com>
2024-02-06 01:13:15 -0300

    Serviços e Sobre nós responsivos!

--- src/app/components/Servicos.jsx
+++ src/app/components/Servicos.jsx
@@ -3,137 +3,47 @@ import Image from "next/image";
 
 const Servicos = () => {
   return (
-    <section className="flex container justify-center max-w-screen-2xl items-center footer-bg mx-auto" id="servicos">
-      <div className="grid grid-cols-6 grid-row-9 items-center flex-wrap justify-center gap-1">
-        <div className="row-start-1 col-span-6 flex justify-center text-blue-ti text-6xl font-extrabold mb-10 mt-10">
-            <h1>Serviços</h1>
-        </div>
-        <div className="row-start-2 col-start-2 col-span-4 flex mb-2">
-            <h1 className="text-blue-ti text-2xl font-bold">Redes</h1>
-        </div>
-        <div className="row-start-3 col-start-2 col-span-4 flex mb-10">
-            <p className=" text-2xl ">Conecte dispositivos de forma eficiente, assegurando comunicação instantânea e compartilhamento ágil de recursos em redes, garantindo desempenho, segurança e adaptabilidade.</p>
-        </div>
-        <div className="row-start-4 col-start-2 col-span-4 row-span-2 grid grid-cols-4 gap-x-12 mb-10">
-            <div className="bg-blue-ti text-white font-semibold rounded-lg py-[6rem] px-5">
-              <div className=" text-white font-semibold grid grid-rows-2">
-                <Image 
-                src="/images/wifi.png"
-                alt="frame"
-                className="object-contain h-full mx-auto"
-                width={80}
-                height={80}
-                />
-                <h1 className="text-center text-2xl mt-10">Mikrotik Routing & Switching</h1>
-              </div>
-            </div>
-            <div className="bg-blue-ti text-white font-semibold rounded-lg py-[6rem] px-5">
-              <div className=" text-white font-semibold grid grid-rows-2">
-                <Image 
-                src="/images/wifi.png"
-                alt="frame"
-                className="object-contain h-full mx-auto"
-                width={80}
-                height={80}
-                />
-                <h1 className="text-center text-2xl mt-10">CISCO Routing & Switching</h1>
-              </div>
-            </div>
-            <div className="bg-blue-ti text-white font-semibold rounded-lg py-[6rem] px-5">
-              <div className=" text-white font-semibold grid grid-rows-2">
-                <Image 
-                src="/images/wifi.png"
-                alt="frame"
-                className="object-contain h-full mx-auto"
-                width={80}
-                height={80}
-                />
-                <h1 className="text-center text-2xl mt-10">HPE Routing & Switching</h1>
-              </div>
-            </div>
-            <div className="bg-blue-ti text-white font-semibold rounded-lg py-[6rem] px-5">
-              <div className=" text-white font-semibold grid grid-rows-2">
-                <Image 
-                src="/images/wifi.png"
-                alt="frame"
-                className="object-contain h-full mx-auto"
-                width={80}
-                height={80}
-                />
-                <h1 className="text-center text-2xl mt-10">Aruba Routing & Switching</h1>
-              </div>
-            </div>
-        </div>
-        <div className="row-start-6 col-start-2 col-span-4 flex mb-2">
-            <h1 className="text-blue-ti text-2xl font-bold">Segurança e Firewall:</h1>
-        </div>
-        <div className="row-start-7 col-start-2 col-span-4 flex mb-10">
-            <p className=" text-2xl ">Fortaleça a proteção da sua rede com soluções de segurança e firewall, assegurando a defesa contra ameaças, controle de tráfego e garantindo ambientes online seguros e resilientes.</p>
-        </div>
-        <div className="row-start-8 col-start-2 col-span-4 row-span-1 grid grid-cols-3 gap-x-12 mb-10">
-            <div className="bg-blue-300 text-white font-semibold rounded py-5 px-10 flex items-center">
-              <Image 
+    <section className="flex flex-col justify-center items-center max-w-screen-2xl footer-bg mx-auto py-10 px-4" id="servicos">
+      <h1 className="text-blue-ti text-4xl md:text-6xl font-extrabold mb-10">Serviços</h1>
+      
+      {/* Seção de Redes */}
+      <h2 className="text-blue-ti text-2xl font-bold mb-4">Redes</h2>
+      <p className="text-xl text-center mb-8">
+        Conecte dispositivos de forma eficiente, assegurando comunicação instantânea e compartilhamento ágil de recursos em redes, garantindo desempenho, segurança e adaptabilidade.
+      </p>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mb-10">
+        {["Mikrotik", "CISCO", "HPE", "Aruba"].map(service => (
+          <div className="bg-blue-ti text-white font-semibold rounded-lg py-12 px-4 flex flex-col items-center justify-center">
+            <Image 
+              src="/images/wifi.png"
+              alt={`${service} Routing & Switching`}
+              width={100}
+              height={100}
+              layout="intrinsic"
+            />
+            <h3 className="text-center text-xl mt-4">{`${service} Routing & Switching`}</h3>
+          </div>
+        ))}
+      </div>
+      
+      {/* Seção de Segurança e Firewall */}
+      <h2 className="text-blue-ti text-2xl font-bold mb-4">Segurança e Firewall:</h2>
+      <p className="text-xl text-center mb-8">
+        Fortaleça a proteção da sua rede com soluções de segurança e firewall, assegurando a defesa contra ameaças, controle de tráfego e garantindo ambientes online seguros e resilientes.
+      </p>
+      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-6 mb-10">
+        {["CISCO Asa", "PFsense", "Mikrotik", "Checkpoint", "Sonicwall", "Fortigate"].map(firewall => (
+          <div className="bg-blue-300 text-white font-semibold rounded py-8 px-4 flex items-center justify-center">
+            <Image 
               src="/images/Frame.png"
-              alt="frame"
-              className="object-contain h-full mr-10"
-              width={40}
-              height={40}
-              />
-              <h1 className="text-center pl-10">CISCO Asa</h1>
-            </div>
-            <div className="bg-blue-300 text-white font-semibold rounded py-5 px-10 flex items-center">
-              <Image 
-              src="/images/Frame.png"
-              alt="frame"
-              className="object-contain h-full mr-10"
-              width={40}
-              height={40}
-              />
-              <h1 className="text-center pl-10">PFsense</h1>
-            </div>
-            <div className="bg-blue-300 text-white font-semibold rounded py-5 px-10 flex items-center">
-              <Image 
-              src="/images/Frame.png"
-              alt="frame"
-              className="object-contain h-full mr-10"
-              width={40}
-              height={40}
-              />
-              <h1 className="text-center pl-10">Mikrotik</h1>
-            </div>
-        </div>            
-        <div className="row-start-9 col-start-2 col-span-4 row-span-1 grid grid-cols-3 gap-x-12 mb-10">
-            <div className="bg-blue-300 text-white font-semibold rounded py-5 px-10 flex items-center">
-              <Image 
-              src="/images/Frame.png"
-              alt="frame"
-              className="object-contain h-full mr-10"
-              width={40}
-              height={40}
-              />
-              <h1 className="text-center pl-10">Checkpoint</h1>
-            </div>
-            <div className="bg-blue-300 text-white font-semibold rounded py-5 px-10 flex items-center">
-              <Image 
-              src="/images/Frame.png"
-              alt="frame"
-              className="object-contain h-full mr-10"
-              width={40}
-              height={40}
-              />
-              <h1 className="text-center pl-10">Sonicwall</h1>
-            </div>
-            <div className="bg-blue-300 text-white font-semibold rounded py-5 px-10 flex items-center">
-              <Image 
-              src="/images/Frame.png"
-              alt="frame"
-              className="object-contain h-full mr-10"
-              width={40}
-              height={40}
-              />
-              <h1 className="text-center pl-10">Fortigate</h1>
-            </div>
-        </div>
+              alt={firewall}
+              width={60}
+              height={60}
+              layout="intrinsic"
+            />
+            <h3 className="text-center text-xl ml-4">{firewall}</h3>
+          </div>
+        ))}
       </div>
     </section>
   );
--- src/app/components/Sobrenos.jsx
+++ src/app/components/Sobrenos.jsx
@@ -3,28 +3,30 @@ import Image from "next/image";
 
 const Sobrenos = () => {
   return (
-    <section className="flex container justify-center max-w-screen-2xl h-dvh items-center bg-blue-sobrenos footer-bg mx-auto" id="sobrenos">
-      <div className="grid grid-cols-6 items-center flex-wrap justify-center gap-14">
-          <div className="col-start-2 col-span-2 items-center w-full h-auto mx-auto">
-            <Image
-              src="/images/sobrenos.png"
-              alt="image"
-              width={490}
-              height={490}
-            />
-          </div> 
-          <div className="col-span-2 aspect-w-3 aspect-h-2 w-full h-auto items-center">
-            <h1 className="h-auto mb-10 flex justify-center mx-auto text-blue-ti text-6xl font-extrabold">
-                Sobre Nós
-            </h1>
-            <p className="text-xl mt-4">
-              Bem-vindo à <b>Absoluto</b>, sua parceira de confiança no universo da Tecnologia da Informação, especializada em soluções de Redes, Routing e Firewall. Fundada em 2023, nossa empresa se destina a prover soluções inovadoras e seguras para otimizar e proteger a infraestrutura de TI de nossos clientes. </p>
-            <p className="text-xl mt-2">
-              <b>Missão:</b> Nossa missão é oferecer soluções de TI personalizadas que garantam a segurança, a eficiência e a escalabilidade dos sistemas de nossos clientes. Entendemos os desafios únicos enfrentados pelas empresas na era digital e estamos dedicados a superá-los, garantindo que suas operações de rede sejam robustas e resilientes. </p>
-            <p className="text-xl mt-2">
-              <b>Visão:</b> Aspiramos ser reconhecidos como líderes no fornecimento de soluções de redes, routing e firewall, estabelecendo padrões de excelência no atendimento ao cliente, inovação e segurança. Estamos empenhados em manter-nos na vanguarda da tecnologia, antecipando as necessidades futuras de nossos clientes e do mercado.
-            </p>
-          </div>  
+    <section className="min-h-screen flex flex-col md:flex-row items-center justify-center bg-blue-sobrenos footer-bg mx-auto px-4 py-8" id="sobrenos">
+      <div className="flex-1 flex justify-center md:justify-end md:pr-8 lg:px-8">
+        <Image
+          src="/images/sobrenos.png"
+          alt="Sobre Nós"
+          width={490}
+          height={490}
+          objectFit="cover"
+          layout="intrinsic"
+        />
+      </div>
+      <div className="flex-1 px-6 md:px-8 lg:px-12">
+        <h1 className="text-4xl md:text-6xl font-extrabold text-blue-ti mb-10 text-center md:text-left mt-2">
+          Sobre Nós
+        </h1>
+        <p className="text-xl mt-4">
+          Bem-vindo à <b>Absoluto</b>, sua parceira de confiança no universo da Tecnologia da Informação, especializada em soluções de Redes, Routing e Firewall. Fundada em 2023, nossa empresa se destina a prover soluções inovadoras e seguras para otimizar e proteger a infraestrutura de TI de nossos clientes.
+        </p>
+        <p className="text-xl mt-2">
+          <b>Missão:</b> Nossa missão é oferecer soluções de TI personalizadas que garantam a segurança, a eficiência e a escalabilidade dos sistemas de nossos clientes. Entendemos os desafios únicos enfrentados pelas empresas na era digital e estamos dedicados a superá-los, garantindo que suas operações de rede sejam robustas e resilientes.
+        </p>
+        <p className="text-xl mt-2">
+          <b>Visão:</b> Aspiramos ser reconhecidos como líderes no fornecimento de soluções de redes, routing e firewall, estabelecendo padrões de excelência no atendimento ao cliente, inovação e segurança. Estamos empenhados em manter-nos na vanguarda da tecnologia, antecipando as necessidades futuras de nossos clientes e do mercado.
+        </p>
       </div>
     </section>
   );
